cab0030e5967ea6ab056c80b3ac76627f08d1825
Lucas Alves <lucasalves_taz@hotmail.com>
2025-04-23 19:59:59 -0300

    up f dags

--- dags/dag_etl.py
+++ dags/dag_etl.py
@@ -3,24 +3,40 @@ from airflow.models import Variable
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.utils.dates import days_ago
 
+# Variável que contém o caminho do banco de dados
 PATH_DATA_DL = Variable.get("PATH_DATA_DL")
 
 
 with DAG(
-    dag_id="create_view_client_email_marketing",
+    dag_id="create_table_cliente_email_marketing",
     description="Cria uma view com resumo de \
     clientes por tipo de e-mail marketing",
-    schedule_interval=None,
+    schedule_interval="@daily",
     start_date=days_ago(1),
     catchup=False,
     template_searchpath="/opt/airflow/sql",
-    tags=["sql", "postgres", "view"],
+    tags=["sql", "postgres", "table"],
 ) as dag:
 
-    create_view_client_email_marketing = PostgresOperator(
-        task_id="create_view_client_email_marketing",
+    create_table_cliente_email_marketing = PostgresOperator(
+        task_id="create_table_cliente_email_marketing",
         postgres_conn_id=PATH_DATA_DL,
-        # Realiza a leitura do arquivo SQL e executa o comando contido nele
-        # O arquivo SQL deve estar na pasta /opt/airflow/sql
-        sql="extract_client_email_marketing.sql",
+        sql="extract_qtd_cliente_email_marketing.sql",
+    )
+
+with DAG(
+    dag_id="create_table_cliente_ativo",
+    description="Cria uma view com resumo de \
+        clientes ativos",
+    schedule_interval="@daily",
+    start_date=days_ago(1),
+    catchup=False,
+    template_searchpath="/opt/airflow/sql",
+    tags=["sql", "postgres", "table"],
+) as dag:
+
+    create_table_cliente_ativo = PostgresOperator(
+        task_id="create_table_cliente_ativo",
+        postgres_conn_id=PATH_DATA_DL,
+        sql="extract_cliente_ativo.sql",
     )
